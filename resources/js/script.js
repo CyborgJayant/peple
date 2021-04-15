fad469adcd8be113c3bd027153b859c817934ac4
Jayant Sharma <sharma.jayant40@gmail.com>
2021-04-15 17:12:25 +0530

    SlideShow added

--- resources/js/script.js
+++ resources/js/script.js
@@ -72,4 +72,46 @@ $(document).ready(function() {
         }        
         
     });
+    
+    /* SlideShow */
+	var slideCount = $('#slider ul li').length;
+	var slideWidth = $('#slider ul li').width();
+	var slideHeight = $('#slider ul li').height();
+	var sliderUlWidth = slideCount * slideWidth;
+	
+	$('#slider').css({ width: slideWidth, height: slideHeight });
+	
+	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	
+    $('#slider ul li:last-child').prependTo('#slider ul');
+
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 0, function () {  //0 is time
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 0, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
+
+    $('a.control_prev').click(function (e) {
+        moveLeft();
+        e.preventDefault();
+    });
+
+    $('a.control_next').click(function (e) {
+        moveRight();
+        e.preventDefault();
+    });
+
 });
+
